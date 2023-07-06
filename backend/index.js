8620c4656daf8ab886ff709b573740532922b952
Kp <kpericles23@hotmail.com>
2023-07-05 23:10:21 -0400

    first attempt

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use('/',require ('./controllers/'))
+app.use('/', require('./controllers/'))
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.get('*', (req,res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+
+const PORT = process.env.PORT
+
+app.listen(PORT, console.log(`listening on port ${PORT}`))
