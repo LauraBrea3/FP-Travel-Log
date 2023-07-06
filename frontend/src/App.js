3e1086001be713964f5d985fe77b9ecfb29bcabe
Laura Brea <laurabrea17@gmail.com>
2023-07-06 19:33:16 -0400

    second commit, frontend and backend up and running as well as db

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function App() {
+  return (
+    <div>Hello World</div>
+  )
+}
