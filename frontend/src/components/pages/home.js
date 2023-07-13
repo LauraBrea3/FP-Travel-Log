5a758ca76a256e1d40b37cb8887f4ebf6fc758fd
Resnick1022 <resnick1022@gmail.com>
2023-07-13 19:58:03 -0400

    squashed some bugs

--- frontend/src/components/pages/home.js
+++ frontend/src/components/pages/home.js
@@ -2,10 +2,12 @@ import React from 'react';
 import '../../App.css';
 import Cards from '../cards';
 import Footer from '../Footer';
+import Navbar from '../navbar';
 
 function Home() {
   return (
     <>
+    <Navbar />
       <Cards />
       <Footer />
     </>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,12 +12,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
 
 // If you want to start measuring performance in your app, pass a function
