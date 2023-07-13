879652c3ecd909ea4632bd79217ed8d6a167b1c6
Resnick1022 <resnick1022@gmail.com>
2023-07-13 19:26:40 -0400

    pushing some changes for the homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import "./App.css";
 import "./main.css";
 import { useState, useEffect } from "react";
-import Navbar from './components/navbar';
-import Home from './components/pages/home';
+// import Navbar from './components/navbar';
+// import Home from './components/pages/home';
 import axios from "axios";
+import Home from "./components/pages/home";
 
 export default function App() {
   const [location, setLocation] = useState([]);
@@ -20,50 +21,6 @@ export default function App() {
     fetchLocations();
   }, []);
   return <div>
-    <div class="container">
-        <form class="form" id="login">
-            <h1 class="form__title">Login</h1>
-            <div class="form__message form__message--error"></div>
-            <div class="form__input-group">
-                <input type="text" class="form__input" autofocus placeholder="Username or email"/>
-                <div class="form__input-error-message"></div>
-            </div>
-            <div class="form__input-group">
-                <input type="password" class="form__input" autofocus placeholder="Password"/>
-                <div class="form__input-error-message"></div>
-            </div>
-            <button class="form__button" type="submit">Continue</button>
-            <p class="form__text">
-                <a href="#" class="form__link">Forgot your password?</a>
-            </p>
-            <p class="form__text">
-                <a class="form__link" href="./" id="linkCreateAccount">Don't have an account? Create account</a>
-            </p>
-        </form>
-        <form class="form form--hidden" id="createAccount">
-            <h1 class="form__title">Create Account</h1>
-            <div class="form__message form__message--error"></div>
-            <div class="form__input-group">
-                <input type="text" id="signupUsername" class="form__input" autofocus placeholder="Username"/>
-                <div class="form__input-error-message"></div>
-            </div>
-            <div class="form__input-group">
-                <input type="text" class="form__input" autofocus placeholder="Email Address"/>
-                <div class="form__input-error-message"></div>
-            </div>
-            <div class="form__input-group">
-                <input type="password" class="form__input" autofocus placeholder="Password"/>
-                <div class="form__input-error-message"></div>
-            </div>
-            <div class="form__input-group">
-                <input type="password" class="form__input" autofocus placeholder="Confirm password"/>
-                <div class="form__input-error-message"></div>
-            </div>
-            <button class="form__button" type="submit">Continue</button>
-            <p class="form__text">
-                <a class="form__link" href="./" id="linkLogin">Already have an account? Sign in</a>
-            </p>
-        </form>
-    </div>
-    <script src="./main.js"></script>
-    
+        <Home />
+</div>
+}
