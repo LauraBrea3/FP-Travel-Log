63f6968dab7f2ef62546029f6e06407872011b09
Resnick1022 <resnick1022@gmail.com>
2023-07-16 09:03:52 -0400

    changes including (most) of map+google maps API

---
+++ frontend/src/components/map.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <title>GeoRSS Layers</title>
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="map"></div>
+    <script>
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: {lat: 49.496675, lng: -102.65625}
+        });
+
+        var georssLayer = new google.maps.KmlLayer({
+          url: '//PUT THE URL
+        });
+        georssLayer.setMap(map);
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap">
+    </script>
+  </body>
+</html>
