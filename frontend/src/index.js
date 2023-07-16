35323b119ba21e8a6889a31f8747cfbc25337b00
Resnick1022 <114195739+Resnick1022@users.noreply.github.com>
2023-07-16 00:21:28 -0400

    Revert "authentication"

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,83 +1,9 @@
-import React, { useState, useEffect, createContext } from "react";
-import axios from "axios";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import LoginForm from "./components/LoginForm";
-import SignUpForm from "./components/SignUpForm";
-import Home from "./components/Home";
-import NotFound from "./components/NotFound"
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-
-
-const AuthContext = createContext();
-
-const AuthContextProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  const setAuth = (value) => {
-    setIsAuthenticated(value);
-  };
-
-  return (
-    <AuthContext.Provider value={{ isAuthenticated, setAuth }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
-
-export default function App() {
-  const [locations, setLocations] = useState([]);
-
-  const fetchLocations = async () => {
-    try {
-      const response = await axios.get("http://localhost:5000/location");
-      setLocations(response.data);
-    } catch (error) {
-      console.log("Error fetching locations:", error);
-    }
-  };
-
-  console.log(locations);
-
-  useEffect(() => {
-    fetchLocations();
-  }, []);
-
-  return (
-    <AuthContextProvider>
-      <div>
-        <Router>
-          <Routes>
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/signup" element={<SignUpForm />} />
-            <Route
-              path="/"
-              element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="*" element={<Home />} /> {/* Handle 404 */}
-          </Routes>
-        </Router>
-      </div>
-    </AuthContextProvider>
-  );
-}
-
-const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated } = React.useContext(AuthContext);
-
-  return isAuthenticated ? children : <Navigate to="/login" />;
-};
-
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
-
-
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -86,3 +12,9 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
