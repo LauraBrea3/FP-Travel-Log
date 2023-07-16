076dcf39328a41b976ee41b7f2457a8e2bda77f3
Kp <kpericles23@hotmail.com>
2023-07-15 20:16:06 -0400

    authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,70 +1,21 @@
-import React from "react";
-import "./App.css";
-import "./main.css";
-import { useState, useEffect } from "react";
-import axios from "axios";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
+import SignUpForm from './components/SignUpForm';
+import Home from './components/Home';
+import PrivateRoute from './components/PrivateRoute';
 
-export default function App() {
-  const [location, setLocation] = useState([]);
-
-  const fetchLocations = async () => {
-    const { data } = await axios.get("/location");
-    setLocation(data);
-  };
-
-  console.log(location);
-
-  useEffect(() => {
-    fetchLocations();
-  }, []);
-  return <div>
-    <div class="container">
-        <form class="form" id="login">
-            <h1 class="form__title">Login</h1>
-            <div class="form__message form__message--error"></div>
-            <div class="form__input-group">
-                <input type="text" class="form__input" autofocus placeholder="Username or email"/>
-                <div class="form__input-error-message"></div>
-            </div>
-            <div class="form__input-group">
-                <input type="password" class="form__input" autofocus placeholder="Password"/>
-                <div class="form__input-error-message"></div>
-            </div>
-            <button class="form__button" type="submit">Continue</button>
-            <p class="form__text">
-                <a href="#" class="form__link">Forgot your password?</a>
-            </p>
-            <p class="form__text">
-                <a class="form__link" href="./" id="linkCreateAccount">Don't have an account? Create account</a>
-            </p>
-        </form>
-        <form class="form form--hidden" id="createAccount">
-            <h1 class="form__title">Create Account</h1>
-            <div class="form__message form__message--error"></div>
-            <div class="form__input-group">
-                <input type="text" id="signupUsername" class="form__input" autofocus placeholder="Username"/>
-                <div class="form__input-error-message"></div>
-            </div>
-            <div class="form__input-group">
-                <input type="text" class="form__input" autofocus placeholder="Email Address"/>
-                <div class="form__input-error-message"></div>
-            </div>
-            <div class="form__input-group">
-                <input type="password" class="form__input" autofocus placeholder="Password"/>
-                <div class="form__input-error-message"></div>
-            </div>
-            <div class="form__input-group">
-                <input type="password" class="form__input" autofocus placeholder="Confirm password"/>
-                <div class="form__input-error-message"></div>
-            </div>
-            <button class="form__button" type="submit">Continue</button>
-            <p class="form__text">
-                <a class="form__link" href="./" id="linkLogin">Already have an account? Sign in</a>
-            </p>
-        </form>
-    </div>
-    <script src="./main.js"></script>
-    
-
-  </div>;
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login" component={LoginForm} />
+        <Route path="/signup" component={SignUpForm} />
+        <PrivateRoute path="/home" component={Home} />
+        <Redirect to="/login" />
+      </Switch>
+    </Router>
+  );
 }
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect, createContext } from "react";
+import axios from "axios";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import LoginForm from "./components/LoginForm";
+import SignUpForm from "./components/SignUpForm";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const AuthContext = createContext();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const AuthContextProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = (value) => {
+    setIsAuthenticated(value);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default function App() {
+  const [locations, setLocations] = useState([]);
+
+  const fetchLocations = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/location");
+      setLocations(response.data);
+    } catch (error) {
+      console.log("Error fetching locations:", error);
+    }
+  };
+
+  console.log(locations);
+
+  useEffect(() => {
+    fetchLocations();
+  }, []);
+
+  return (
+    <AuthContextProvider>
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/signup" element={<SignUpForm />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="*" element={<Home />} /> {/* Handle 404 */}
+          </Routes>
+        </Router>
+      </div>
+    </AuthContextProvider>
+  );
+}
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = React.useContext(AuthContext);
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
+
+
+
+
+
+
